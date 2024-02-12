55f82cf683eff274aefa644415b725d1b91f43f8
Daria Dontsova <dariallab@gmail.com>
2024-02-12 17:48:55 +0100

    Add log

--- main.go
+++ main.go
@@ -2,25 +2,28 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"os"
 
 	"github.com/kelseyhightower/envconfig"
+	"github.com/rs/zerolog"
 )
 
 type Config struct {
-	Port int `envconfig:"PORT" required:"true" default:"8181"`
+	Port int `envconfig:"PORT" required:"true"`
 }
 
 func main() {
+	l := zerolog.New(os.Stdout)
+
 	var cfg Config
 	if err := envconfig.Process("", &cfg); err != nil {
-		log.Fatalf("can't load confg: %v", err)
+		l.Fatal().Err(err).Msg("can't load confg")
 	}
 
 	server := &Server{}
-
+	l.Info().Int("port", cfg.Port).Msg("starting server")
 	if err := http.ListenAndServe(fmt.Sprintf(":%v", cfg.Port), server); err != nil {
-		log.Fatalf("could not start server: %v", err)
+		l.Fatal().Err(err).Msg("can't start server")
 	}
 }
