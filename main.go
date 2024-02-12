f79fbe980f02d83012bc656434914d7115322e6c
Daria Dontsova <dariallab@gmail.com>
2024-02-12 17:32:09 +0100

    Add env

--- main.go
+++ main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/kelseyhightower/envconfig"
 )
 
+type Config struct {
+	Port int `envconfig:"PORT" required:"true" default:"8181"`
+}
+
 func main() {
+	var cfg Config
+	if err := envconfig.Process("", &cfg); err != nil {
+		log.Fatalf("can't load confg: %v", err)
+	}
+
 	server := &Server{}
 
-	if err := http.ListenAndServe(":8181", server); err != nil {
+	if err := http.ListenAndServe(fmt.Sprintf(":%v", cfg.Port), server); err != nil {
 		log.Fatalf("could not start server: %v", err)
 	}
 }
