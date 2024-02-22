fdc6f78d32c9e5c9be19bb4d49506d0ae0815416
Daria Dontsova <dariallab@gmail.com>
2024-02-22 19:13:57 +0100

    Move tailwind

--- pkg/templates/static/embed.go
+++ pkg/templates/static/embed.go
@@ -2,5 +2,5 @@ package static
 
 import "embed"
 
-//go:embed *
+//go:embed output.css
 var FS embed.FS
--- pkg/templates/tailwind.config.js
+++ pkg/templates/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "pkg/templates/**/*.html",
+    "**/*.html"
   ],
   theme: {
     extend: {},
